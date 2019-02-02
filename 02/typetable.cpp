7d91c8c0b8e8948295449b7e267bdfc9218c80b4
yushkov-i <47275068+yushkov-i@users.noreply.github.com>
2019-02-02 17:31:20 +0300

    Добавлено д/з 2

---
+++ 02/typetable.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <limits>
+
+/* TODO
+Используя заголовочный файл <limits>, std::cout, sizeof() и управляющие последовательности
+cоставьте следующую таблицу
+
+	Тип переменной		|	Размер,	|			Значение				|	Количество
+						|	байт	|	Минимальное	|	Максимальное	|	значимых бит
+	bool				|	1		|	false		|	true			|	1
+	unsigned short		|	2		|	0			|	65535			|	16
+	short
+	unsigned int
+	int
+	unsigned long
+	long
+	unsigned long long
+	long long
+	char
+	float
+	double
+
+для справки использовать http://www.cplusplus.com/reference/limits/numeric_limits/
+*/
+
+int main()
+{
+	setlocale(0, ""); // включение адекватного отображения кириллицы в консоль
+	std::cout << std::boolalpha; // настройка консоли, вывод булевых типов как true / false
+	std::cout << "Максимальное значение переменной типа int равно " << std::numeric_limits<int>::max(); // пример использования <limits>
+}
